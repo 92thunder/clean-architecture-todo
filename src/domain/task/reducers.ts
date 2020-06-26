0432d4d744aabae00673f4f66d29fdbea5046e98
ryota.kunisada <r.kunisada661@gmail.com>
2020-06-27 01:56:35 +0900

    setState修正

--- src/domain/task/reducers.ts
+++ src/domain/task/reducers.ts
@@ -3,6 +3,6 @@ import { Task, TaskState } from './types'
 export function setState(task: Task, taskState: TaskState): Task {
   return {
     ...task,
-    ...{ state: taskState }
+    state: taskState
   }
 }
